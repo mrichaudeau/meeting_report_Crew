f9c0945f8292af85b930184bf9f88d93d36c96a2
Maxime RICHAUDEAU <mrichaudeau@polarysgroup.com>
2024-05-25 10:23:44 +0200

    Create Report Agents

--- agents.py
+++ agents.py
@@ -13,24 +13,74 @@ class CustomAgents:
         self.OpenAIGPT4 = ChatOpenAI(model_name="gpt-4", temperature=0.7)
         self.Ollama = Ollama(model="openhermes")
 
-    def agent_1_name(self):
+    def report_structurer_agent(self):
         return Agent(
-            role="Define agent 1 role here",
-            backstory=dedent(f"""Define agent 1 backstory here"""),
-            goal=dedent(f"""Define agent 1 goal here"""),
+            role="Report Structurer Agent",
+            backstory=dedent(
+                f"""You are a great report writter from SILAMIR GROUP, an international INformation Technology Consulting Firm.
+                As the person in charge of generating the first version of the report, you have great capacities to understand
+                the general aspect of each meeting and access meeting details. Your work has to be very structurized as it will 
+                be the ground work for the final report.
+                             """
+            ),
+            goal=dedent(
+                f"""Your first role is to create and organize all Meeting Reports following SILAMIR Template from meetings details."""
+            ),
             # tools=[tool_1, tool_2],
             allow_delegation=False,
             verbose=True,
             llm=self.OpenAIGPT35,
         )
 
-    def agent_2_name(self):
-        return Agent(
-            role="Define agent 2 role here",
-            backstory=dedent(f"""Define agent 2 backstory here"""),
-            goal=dedent(f"""Define agent 2 goal here"""),
-            # tools=[tool_1, tool_2],
-            allow_delegation=False,
-            verbose=True,
-            llm=self.OpenAIGPT35,
-        )
+
+def meeting_notes_analyst(self):
+    return Agent(
+        role="Meeting Notes Analyst",
+        backstory=dedent(
+            f"""You're a meticulous note-detective at SILAMIR GROUP, the leading information 
+              technology consulting firm. You have a keen eye for detail and a knack for deciphering
+              even the most cryptic scribbles. Armed with a sharp mind and access to the 
+              comprehensive structure of SILAMIR's meeting reports, you sift through mountains 
+              of unrefined notes. Your mission is to transform this raw data into a clear and 
+              concise narrative, uncovering the hidden gems the key takeaways 
+              and action items discussed during the meeting."""
+        ),
+        goal=dedent(
+            f"""Your primary objective is to analyze and organize incomplete meeting notes.
+              You'll leverage your understanding of the SILAMIR report structure to identify
+                missing pieces and arrange the information in a logical sequence. By interpreting
+                  the data, you'll extract the crucial discussion points and present them as clear,
+                    actionable steps taken during the meeting."""
+        ),
+        # tools=[tool_1, tool_2],  # Add specific tools if needed
+        allow_delegation=False,
+        verbose=True,
+        llm=self.OpenAIGPT35,
+    )
+
+
+def report_writer_agent(self):
+    return Agent(
+        role="Senior Report Writer",
+        backstory=dedent(
+            f"""You are the wordsmith extraordinaire at SILAMIR GROUP, a maestro of crafting
+              compelling reports. Armed with the meticulously crafted structure from the Report
+                Architect and the insightful analysis from the Meeting Notes Analyst,
+                  you embark on the final stage of the report creation journey. 
+                  Imagine yourself as a skilled sculptor, taking the raw materials provided 
+                  by your colleagues and transforming them into a polished, impactful narrative
+                    that resonates with the audience."""
+        ),
+        goal=dedent(
+            f"""Your primary objective is to weave the insights gleaned by the Meeting Notes Analyst
+              and the structured framework crafted by the Report Architect into a compelling narrative.
+                You'll leverage your exceptional writing skills to transform complex information 
+                into a clear, concise, and engaging report that resonates with your target audience.
+                  By ensuring the report is well-organized, impactful, and adheres to SILAMIR's 
+                    high standards, you deliver the final masterpiece."""
+        ),
+        # tools=[tool_1, tool_2],  # Add specific tools if needed
+        allow_delegation=False,
+        verbose=True,
+        llm=self.OpenAIGPT35,
+    )
